1124d08a650bd147bbb3f3e8b473a0e62fb019f3
Pedro Arvellos <pedroarvellos@hotmail.com>
2018-09-20 08:57:31 -0300

    fix: Name of the props in Search Component was wrong

--- src/Search/Search.jsx
+++ src/Search/Search.jsx
@@ -4,7 +4,7 @@ import './Search.css'
 
 class Search extends Component {
   render() {
-    const { onToggleOpen, onInputChanged, places } = this.props;
+    const { onToggle, onInputChanged, places } = this.props;
 
     this.renderPlaces = place => {
       return (<ListGroupItem tabindex={'0'} className={ 'nav-option hover-option' } cursor={ 'pointer' } key={ place.id } onClick={ () => onToggle(place.id, true) } onKeyPress={ () => onToggle(place.id, true) }>{ place.name }</ListGroupItem>)
